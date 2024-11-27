672b34a14040a316f3717dc18df64d7f37a76bdb
bharathreddyt955 <bharathreddyt6302@gmail.com>
2024-11-27 16:03:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,129 @@
+function redText(){
+let col=document.getElementById("changecolor");
+col.style.color="red";
+}
+
+function greenText(){
+ let col=document.getElementById("changecolor");
+    col.style.color="green";
+    }
+
+    function yellowText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="yellow";
+        }     
+        function blueText(){
+            let col=document.getElementById("changecolor");
+            col.style.color="blue";
+       }
+
+       function violetText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="violet";
+    }
+    function indigoText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="indigo";
+    }
+    function pinkText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="pink";
+    }
+    function cyanText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="cyan";
+ }
+ function blackText(){
+    let col=document.getElementById("changecolor");
+    col.style.color="black";
+    }
+    function greyText(){
+        let col=document.getElementById("changecolor");
+        col.style.color="grey";
+        }
+        function redback(){
+            let col=document.getElementById("changecolor");
+            col.style.background="red";
+            }
+            
+            function greenback(){
+             let col=document.getElementById("changecolor");
+                col.style.background="green";
+                }
+            
+                function yellowback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="yellow";
+                    }     
+                    function blueback(){
+                        let col=document.getElementById("changecolor");
+                        col.style.background="blue";
+                   }
+            
+                   function violetback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="violet";
+                }
+                function indigoback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="indigo";
+                }
+                function pinkback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="pink";
+                }
+                function cyanback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="cyan";
+             }
+             function blackback(){
+                let col=document.getElementById("changecolor");
+                col.style.background="black";
+                }
+                function greyback(){
+                    let col=document.getElementById("changecolor");
+                    col.style.background="grey";
+                    }
+
+                function arial(){
+                    //document.write("hello")
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='arial';
+                }
+                function timesnew(){
+                    
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Times New Roman';
+                }
+                function georgia(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='georgia';
+                }
+                function Garamond(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Garamond';
+                }
+                function CourierNew(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Courier New';
+                }
+                function geoBrushScriptMT(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Brush Script MT';
+                }
+                function Copperplate(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Copperplate';
+                }
+                function cursive(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='cursive';
+                }
+                function serif(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='serif';
+                }
+                function Tahoma(){
+                    let data=document.getElementById('changecolor');
+                    data.style.fontFamily='Tahoma';
+                }
